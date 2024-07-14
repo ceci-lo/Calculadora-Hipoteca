7aec58f379e9ef20f57c45b6f55126b63c197d7c
Cecilia <lcecilia8147@gmail.com>
2024-07-14 15:35:21 -0300

    Se añade el codiog para cuando se quiere obtener el interes o el total

--- js/script.js
+++ js/script.js
@@ -2,18 +2,28 @@ console.warn("HELLO word");
 let capitalPrestado = document.getElementById("mAmoung");
 let years = document.getElementById("mTerm");
 let interest = document.getElementById("iRate");
-
+let radioRepayment = document.getElementById("repayment");
+let radioInterestOnly = document.getElementById("InterestOnly");
 
 let calculate = document.getElementById("btn");
 
-calculate.addEventListener("click", (e)=> { 
-   let month = years.value * 12;
-   console.log("Month = " , month);
-   let TIN = interest.value / 12 ;
-   console.log("TIN = " , TIN);
-   // console.log(capitalPrestado.value * (Math.pow(1+(TIN/100), month)*(TIN/100) / (Math.pow(1+(TIN/100) - 1))));
-let TINt = TIN/100;
-let multiplier =  (Math.pow(1 + TINt, month) * TINt) / (Math.pow(1 + TINt, month)-1) ;
-let total = capitalPrestado.value * multiplier;
-   console.log("sacando total : ", total * month );
+calculate.addEventListener("click", (e) => {
+  let month = years.value * 12;
+  let TIN = interest.value / 12;
+  let TINt = TIN / 100;
+  let multiplier =
+    (Math.pow(1 + TINt, month) * TINt) / (Math.pow(1 + TINt, month) - 1);
+  let total = capitalPrestado.value * multiplier;
+
+  let repayment = Math.round(total);
+  let interestOnly = Math.round(
+    (repayment * month - capitalPrestado.value) / 360
+  );
+
+  if (radioRepayment.checked) {
+    console.log("repayment : ", repayment);
+    console.log("radio repayment : ", radioRepayment.checked);
+  } else if (radioInterestOnly.checked) {
+    console.log("Intertest Only : ", interestOnly);
+  }
 });
