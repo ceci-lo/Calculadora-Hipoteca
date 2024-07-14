fe78f30b42999094b099e812b240a916b1dfd7c6
Cecilia <lcecilia8147@gmail.com>
2024-07-14 17:11:45 -0300

    se crea funcion mostrar aviso para mostrar los nuevos titulos una vez enviado el formulario

--- js/script.js
+++ js/script.js
@@ -19,11 +19,32 @@ calculate.addEventListener("click", (e) => {
   let interestOnly = Math.round(
     (repayment * month - capitalPrestado.value) / 360
   );
-
+mostrarAviso();
   if (radioRepayment.checked) {
     console.log("repayment : ", repayment);
-    console.log("radio repayment : ", radioRepayment.checked);
   } else if (radioInterestOnly.checked) {
     console.log("Intertest Only : ", interestOnly);
   }
 });
+
+function mostrarAviso() {
+  let oldTitle = document.getElementsByClassName("container2__results_title")[0];
+  let oldP = document.getElementsByClassName("container2__results_p")[0];
+  let img = document.getElementsByClassName("container2__results__img")[0];
+
+  img.remove();
+  oldTitle.innerText = "Your results";
+  oldP.innerText =
+    'Your results shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again';
+
+    //Estilos
+   // Titulo 
+
+   oldTitle.style.textAlign = "left";
+   oldP.style.textAlign = "left";
+   oldP.style.padding = "0px 3px";
+
+
+}
+
+function mostrarResultado() {}
